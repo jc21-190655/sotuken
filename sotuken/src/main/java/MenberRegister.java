487a82120aa778d516dc62a98f2c1ddd7980958c
190151 <190151@e0505-40.JC-21.local>
2021-12-10 16:34:10 +0900

    sql追加

--- sotuken/src/main/java/MenberRegister.java
+++ sotuken/src/main/java/MenberRegister.java
@@ -37,6 +37,8 @@ public class MenberRegister extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		request.getParameter(getServletInfo())
+		
 	}*/
 
 }
